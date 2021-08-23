838ba049c97010646e0863e7ed572248c9c67dab
Hugo Obuchi <76051977+hugo-obuchi@users.noreply.github.com>
2021-08-23 16:54:09 +0900

    Initial dotnet console project commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AGLW_CSharp_BasicChatClientSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
